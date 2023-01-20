7205f17f3648ce57ad9815091279e21ec09ca248
Darpeex <92158213+Darpeex@users.noreply.github.com>
2023-01-20 09:39:07 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 600) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
+        }
+        // if (window.pageYOffset > 962) {
+        //     header.classList.add('header_active2');
+        // } else {
+        //     header.classList.remove('header_active2');
+        // }
+    }
+} ());
+
+// Burger handler
+
+    (function() {
+        const burgerItem = document.querySelector('.burger');
+        const menu = document.querySelector ('.header_-nav');
+        const menuCloseItem = document.querySelector('.header__nav-close')
+        burgerItem.addEventListener('click', () => {
+            menu.classList.add('header__nav-active');
+        });
+        menuCloseItem.addEventListener('click', () => {
+            menu.classList.remove('header__nav-active');
+        })
+    }());
